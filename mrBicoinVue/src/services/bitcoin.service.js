d2da4fe5a6021b3a2fedd1eaa6724d7e6005e051
yinonab <yinonabarjel@gmail.com>
2023-12-01 13:31:47 +0200

    F/limited response data to 20

--- mrBicoinVue/src/services/bitcoin.service.js
+++ mrBicoinVue/src/services/bitcoin.service.js
@@ -1,66 +1,73 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const KEY = 'bitcoinData'; // Key for storing data in local storage
+const KEY = 'bitcoinData' // Key for storing data in local storage
 
 export const bitcoinService = {
     async getRate(balance) {
         try {
-            let data = localStorage.getItem(KEY);
+            let data = localStorage.getItem(KEY)
             if (data) {
-                data = JSON.parse(data);
+                data = JSON.parse(data)
                 if (data.rate) {
-                    return data.rate;
+                    return data.rate
                 }
             }
     
-            const response = await axios.get(`https://blockchain.info/tobtc?currency=USD&value=${balance}`);
-            localStorage.setItem(KEY, JSON.stringify({ ...data, rate: response.data }));
-            console.log('response.data:', response.data);
-            return response.data;
+            const response = await axios.get(`https://blockchain.info/tobtc?currency=USD&value=${balance}`)
+            localStorage.setItem(KEY, JSON.stringify({ ...data, rate: response.data }))
+            console.log('response.data:', response.data)
+            return response.data
         } catch (error) {
-            console.error('Error fetching exchange rates:', error);
-            return null;
+            console.error('Error fetching exchange rates:', error)
+            return null
         }
     },
     
 
     async getMarketPriceHistory() {
         try {
-            let data = localStorage.getItem(KEY);
+            let data = localStorage.getItem(KEY)
             if (data) {
-                data = JSON.parse(data);
+                data = JSON.parse(data)
                 if (data.marketPriceHistory) {
-                    return data.marketPriceHistory;
+                    // Extract the first 20 values from the stored data
+                    const first20Values = data.marketPriceHistory.values.slice(0, 20)
+                    return { ...data.marketPriceHistory, values: first20Values }
                 }
             }
-            const response = await axios.get('https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true');
-            console.log('Market Price History Response:', response.data); // Log the response data
-            localStorage.setItem(KEY, JSON.stringify({ ...data, marketPriceHistory: response.data }));
-            return response.data;
+    
+            const response = await axios.get('https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true')
+            console.log('Market Price History Response:', response.data) // Log the response data
+    
+            // Store only the first 20 values in localStorage
+            const first20Values = response.data.values.slice(0, 20)
+            localStorage.setItem(KEY, JSON.stringify({ ...data, marketPriceHistory: { ...response.data, values: first20Values } }))
+            
+            return { ...response.data, values: first20Values }
         } catch (error) {
-            console.error('Error fetching market price history:', error);
-            return null;
-        }
+            console.error('Error fetching market price history:', error)
+            return null
+    }
     },
     
 
     async getAvgBlockSize() {
         try {
-            let data = localStorage.getItem(KEY);
+            let data = localStorage.getItem(KEY)
             if (data) {
-                data = JSON.parse(data);
+                data = JSON.parse(data)
                 if (data.avgBlockSize) {
-                    return data.avgBlockSize;
+                    return data.avgBlockSize
                 }
             }
             
-            const response = await axios.get('https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true');
-            localStorage.setItem(KEY, JSON.stringify({ ...data, avgBlockSize: response.data }));
-            console.log('response.data:', response.data);
-            return response.data;
+            const response = await axios.get('https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true')
+            localStorage.setItem(KEY, JSON.stringify({ ...data, avgBlockSize: response.data }))
+            console.log('response.data:', response.data)
+            return response.data
         } catch (error) {
-            console.error('Error fetching average block size:', error);
-            return null;
+            console.error('Error fetching average block size:', error)
+            return null
         }
     },
-};
+}
