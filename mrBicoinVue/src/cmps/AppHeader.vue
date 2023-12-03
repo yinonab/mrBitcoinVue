ca076417bab92ea8f0c2a76a311610db5349b2f5
yinonab <yinonabarjel@gmail.com>
2023-12-03 17:41:59 +0200

    F/fix icons of header

--- mrBicoinVue/src/cmps/AppHeader.vue
+++ mrBicoinVue/src/cmps/AppHeader.vue
@@ -2,9 +2,10 @@
     <header class="app-header">
         <a href="/"><h2>MrBitcoin</h2></a>
         <nav>
-        <RouterLink to="/contact">Contact</RouterLink> |
-        <RouterLink to="/">Home</RouterLink> |
-        <RouterLink to="/statistics">Statistics</RouterLink> |
+        <RouterLink to="/contact"><img class="icons" src="../assets/imgs/users.png" alt=""></RouterLink> 
+        <RouterLink to="/"><img class="icons" src="../assets/imgs/home.png" alt=""></RouterLink> 
+        <RouterLink to="/statistics"><img class="icons" src="../assets/imgs/increase.png" alt=""></RouterLink> 
+       
       </nav>
     </header>
 </template>
@@ -19,9 +20,15 @@
   display: flex;
   align-items: center;
   justify-content: space-between;
-  height: 60px; /* Adjust the height as needed */
+  height: 6em; /* Adjust the height as needed */
   padding: 0 20px;
   background-color: #34495e; /* Change the background color */
+  .icons{
+    padding: .1em;
+    margin-left: 1em;
+    height: 3em;
+    width: 3em;
+  }
 
   a {
     font-size: 1.2rem;
@@ -36,6 +43,17 @@
 
   .router-link-exact-active {
     font-weight: bold; /* Highlight active link */
+  }
+}
+@media (max-width: 600px) {
+    .app-header{
+        .icons{
+    padding: .1em;
+    margin-left: 1em;
+    height: 2em;
+    width: 2em;
+    }
+    
   }
 }
 </style>
